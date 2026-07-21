e34fce6d07ae8599ae160281d06aba8d2b9f3800
unknown <hasnataliawan088@gmail.com>
2026-07-21 16:40:54 +0500

    Refactor Sidebar component styling and structure; update CSS for improved layout and consistency

--- src/components/common/Sidebar.jsx
+++ src/components/common/Sidebar.jsx
@@ -1,29 +1,43 @@
+import { NavLink } from "react-router-dom";
 import hooksData from "../../data/hooksData";
 import "./Sidebar.css";
-import { NavLink } from "react-router-dom";
 
 function Sidebar() {
     return (
         <aside className="sidebar">
-            <h2 className="sidebar-title">📚 React Hooks</h2>
+
+
 
             {hooksData.map((section) => (
-                <div key={section.category} className="sidebar-section">
-                    <h3>{section.category}</h3>
+                <section
+                    key={section.category}
+                    className="sidebar-section"
+                >
+                    <h3 className="section-title">
+                        {section.category}
+                    </h3>
 
-                    <ul>
+                    <ul className="sidebar-list">
+
                         {section.hooks.map((hook) => (
                             <li key={hook.name}>
                                 <NavLink
                                     to={hook.path}
-                                    className="sidebar-link">
+                                    className={({ isActive }) =>
+                                        isActive
+                                            ? "sidebar-link active"
+                                            : "sidebar-link"
+                                    }
+                                >
                                     {hook.name}
                                 </NavLink>
                             </li>
                         ))}
+
                     </ul>
-                </div>
+                </section>
             ))}
+
         </aside>
     );
 }
